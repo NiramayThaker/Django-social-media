3692f0c9508712884d924c4b3a5b23cb9d0e01da
Niramay Thaker <thakerniramay@gmail.com>
2023-01-23 18:10:56 +0530

    Authentication Models and Media Files Routing

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+	# user = pass
+	# id_user = pass
+	# bio = pass
+	profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-photo.png')
+	# location = pass
+
